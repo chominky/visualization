7a71509073f841bbe801c30dd7079bfb535fb1f0
DESKTOP-15QJCF3\airi <mkcho@airi.kr>
2023-05-02 14:24:20 +0900

    data from yfinance

--- visualizer.py
+++ visualizer.py
@@ -4,29 +4,27 @@ import seaborn as sns
 import os
 import plotly.graph_objects as go
 from typing import List
+from pandas_datareader import data as pdr
+import yfinance as yf
+yf.pdr_override()
 
 
-def draw_single_graph(stock: str, x: str, y: str, file_format: str, title: str, save: bool) -> None:
+def draw_single_graph(stock: str, y: str, file_format: str, title: str, save: bool) -> None:
     """Draw graph from csv file
 
     Args:
         stock (str): stock to draw graph
-        x (str): column to x
         y (str): column to y
         format (str): file format. png, html
         title (str): title of plot
         save (bool): whether save or not
     """
-    df = pd.read_csv(f"./data/{stock}.csv")
-    assert x in df.columns, f"{x} column is not in {stock}"
-    assert y in df.columns, f"{y} column is not in {stock}"
+    df = pdr.get_data_yahoo(stock, start="2000-01-01", end="2023-04-27")
 
-    if x == "date":
-        df["date"] = pd.to_datetime(df['date'])
     if file_format == "png":
         plt.figure(figsize=(14,5))
         sns.set_style("ticks")
-        sns.lineplot(data=df, x=x, y=y, color='firebrick')
+        sns.lineplot(data=df, x=df.index, y=y, color='firebrick')
         sns.despine()
         plt.title(title,size='x-large',color='blue')
         plt.grid()
@@ -35,25 +33,24 @@ def draw_single_graph(stock: str, x: str, y: str, file_format: str, title: str, 
             plt.savefig(f"./plots/{title}.png")
     elif file_format == "html":
         fig = go.Figure()
-        fig.add_trace(go.Scatter(name="close",
-                         x=df["date"],
-                         y=df["close"],
+        fig.add_trace(go.Scatter(name="Adj Close",
+                         x=df.index,
+                         y=df["Adj Close"],
                          mode="lines"))
-        fig.update_layout(title_text="The Stock Price of AAPL",
+        fig.update_layout(title_text=f"The Adj Close of {stock}",
                           title_x=0.5)
         fig.update_xaxes(title='date')
-        fig.update_yaxes(title='close')
+        fig.update_yaxes(title='Adj Close')
         fig.show()
         if save:
             os.makedirs("./plots", exist_ok=True)
             fig.write_html(f"./plots/{title}.html")
 
-def draw_multiple_graph(stock_list: List[str], x: str, y: str, file_format: str, save: bool) -> None:
+def draw_multiple_graph(stock_list: List[str], y: str, file_format: str, save: bool) -> None:
     """Draw multiple graph from csv files
 
     Args:
         stock_list (List[str]): stock list to draw graph
-        x (str): column to x
         y (str): column to y
         format (str): file format. png, html. if html, draw with animation bar
         save (bool): whether save or not
@@ -64,14 +61,9 @@ def draw_multiple_graph(stock_list: List[str], x: str, y: str, file_format: str,
         sns.set(style="ticks")
         sns.despine()
         for i, stock in enumerate(stock_list):
-            df = pd.read_csv(f"./data/{stock}.csv")
-            assert x in df.columns, f"{x} column is not in {stock}"
-            assert y in df.columns, f"{y} column is not in {stock}"
-
-            if x == "date":
-                df["date"] = pd.to_datetime(df['date'])
-            sns.lineplot(data=df,x="date",y='close',color='firebrick', ax=axs[i])
-            axs[i].set_title(f"The Stock Price of {stock}",size='x-large',color='blue')
+            df = pdr.get_data_yahoo(stock, start="2000-01-01", end="2023-04-27")
+            sns.lineplot(data=df,x=df.index, y='Adj Close',color='firebrick', ax=axs[i])
+            axs[i].set_title(f"The Adj Close of {stock}",size='x-large',color='blue')
             axs[i].grid()
         plt.tight_layout(pad=1)
         plt.show()
@@ -80,9 +72,9 @@ def draw_multiple_graph(stock_list: List[str], x: str, y: str, file_format: str,
             fig.savefig(f"./plots/{', '.join(stock_list)}.png")
     elif file_format == "html":
         start_visible = [True] + [False for _ in range(len(stock_list)-1)]
-        stock_df_list = [pd.read_csv(f"./data/{stock}.csv") for stock in stock_list]
-        trace_list = [go.Scatter(name=stock,x=df["date"],y=df["close"],mode="lines",visible=vis)
-                    for stock, df, vis in zip(stock_list, stock_df_list, start_visible)]
+        stock_df_list = [pdr.get_data_yahoo(stock, start="2000-01-01", end="2023-04-27") for stock in stock_list]
+        trace_list = [go.Scatter(name=stock,x=df.index,y=df["Adj Close"],mode="lines",visible=vis)
+                      for stock, df, vis in zip(stock_list, stock_df_list, start_visible)]
         fig = go.Figure(data=trace_list)
 
         steps = []
@@ -91,27 +83,27 @@ def draw_multiple_graph(stock_list: List[str], x: str, y: str, file_format: str,
             step = dict(
                         method='update',
                         args=[{'visible': [True if stock in data['name'] else False for data in fig.data]},
-                            {'title.text': f'The Stock Price of {stock}'}],
+                            {'title.text': f'The Adj Close of {stock}'}],
                         label=stock
                     )
             # Add step to step list
             steps.append(step)
         sliders = [dict(steps=steps)]
         fig.layout.sliders = sliders
-        fig.update_layout(title_text=f'The Stock Price of {stock_list[0]}',
+        fig.update_layout(title_text=f'The Adj Close of {stock_list[0]}',
                         title_x=0.5)
         fig.update_xaxes(title='date')
-        fig.update_yaxes(title='close')
+        fig.update_yaxes(title='Adj Close')
         fig.show()
         if save:
             os.makedirs("./plots", exist_ok=True)
             fig.write_html(f"./plots/{', '.join(stock_list)}.html")
 
 if __name__ == "__main__":
-    company_name = "AAPL"
-    draw_single_graph(stock=company_name, x="date", y="close",
+    stock = "AAPL"
+    draw_single_graph(stock=stock, y="Adj Close",
                       file_format="png",
-                      title=f"The Stock Price of {company_name}", save=True)
+                      title=f"The Adj Close of {stock}", save=True)
     # company_list = ["AAPL", "MSFT"]
-    # draw_multiple_graph(stock_list=company_list, x="date", y="close",
+    # draw_multiple_graph(stock_list=company_list, y="Adj Close",
     #                     file_format="png", save=True)
